2dfaaadc7df2f0fbfab578f0d5dfd9d19e2eee43
Priti-i <pritiy049@gmail.com>
2026-05-12 21:21:58 +0530

    schema level validation

--- app.js
+++ app.js
@@ -5,7 +5,34 @@ const mongoose = require('mongoose');
 const mongoURI=require("./src/config/database");
 
 const User=require("./src/models/user");
-app.user(express.json());
+app.use(express.json());
+
+//get all user
+app.get("/getuser",async(req,res)=>{
+  const userEmail=req.body.email;
+  const user=await User.findOne({email:userEmail});
+  if(!user)
+  {
+    res.status(404).send("user not found");
+  }
+  else{
+      res.send(user);
+  }
+})
+//get api
+app.get("/getAllUser",async(res,req)=>{
+
+  try{
+    const users=await User.find({});
+    req.send(users);
+
+  }catch(error)
+  {
+    console.error(error);
+    req.status(500).send("somethinf went wrong")
+  }
+})
+// post api
 app.post("/signup",async(req,res)=>{
     const user=new User(req.body);
     try{
@@ -19,6 +46,32 @@ app.post("/signup",async(req,res)=>{
     }
     
 })
+
+//delete Api by id
+app.delete("/deleteUser",async(req,res)=>{
+  const userId=req.body.id;
+  try{
+    const deleteUser=await User.findByIdAndDelete(userId);
+    res.send("user deleted")
+  }catch(error)
+  {
+    res.status(500).send("error deleting user");
+  }
+})
+
+// update apin data
+app.patch("/updateUser",async(req,res)=>{
+  const userId=req.body.userId;
+  const updateData=req.body;
+  try{
+    const user=await User.findByIdAndUpdate({_id:userId}, updateData,  {runValidators: true });
+    res.send("update data");
+  }catch(error)
+  {
+    res.status(500).send("error updating user",error.message);
+  }
+})
+
 
 
 mongoose.connect(mongoURI)
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,51 @@ const mongoose=require("mongoose");
 const userSchema=mongoose.Schema({
     firstName:{
         type:String,
+        require:true,
+        minlength:4,
+        maxlength:40,
     },
     lastName:{
-        type:String
+        type:String,
     },
     email:{
-      type:String
+      type:String,
+      require:true,
+      lowercase:true,
+      unique:true,
+      trim:true,
     },
     password:{
         type:String
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
+        max:50
     },
     gender:{
-        type:String
+        type:String,
+        validate(value)
+        {
+            if(!["male","female","other"].includes(value))
+            {
+                throw new Error("this is not proper gender");
+            }
+
+        }
+
+    },
+    About:{
+         type:String,
+         default:"default description in your peofile",
+    },
+    profileUrl:{
+        type:String,
+        default:"https://www.flaticon.com/free-icon/profile_3135715"
     }
     
+},{
+    timestamp:true
 });
 const User=mongoose.model("user",userSchema);
 module.exports=User;
