982cdb84c2685f6b59bc685f46bc4c51c5c431a7
Priti Yadav <pritiy049@gmail.com>
2026-06-30 12:19:01 +0530

    change frontend url

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,31 +1,4 @@
-// const jwt=require("jsonwebtoken");
-// const User=require("../models/user");
-// const userAuth=async(req,res,next)=>{
-// //read the token from req cookies
-// try{
-
-// const {token}=req.cookies;
-// if(!token)
-// {
-//    res.status(401).send("user is not logged in");
-// }
-// const decodedMessage=await jwt.verify(token,"@webtindertoken"); // verify token
-// const {_id}=decodedMessage;
-// console.log("after token created ", decodedMessage);
-// const user= await User.findById(_id);
-// if(!user){
-//     throw new Error("user not found");
-// }
-
-// req.user=user; 
-// next();
-// }catch(error)
-// {
-//     res.status(400).status(error.message);
-// }
-
-// }
-// module.exports={userAuth};
+   
 
 const jwt = require("jsonwebtoken");   //here Import jsonwebtoken package
 const User = require("../models/user"); // import user model(user table)
